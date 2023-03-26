532d11fb44ba141f12118596b333208ae6425cdd
Martin Ferreira <martin.ferreira@e-core.com>
2023-03-26 17:34:26 -0300

    initial commit

---
+++ src/mocks/Home.js
@@ -0,0 +1,9 @@
+
+
+const home = {
+  header: {
+    titulo: "Hey I'm here",
+  },
+}
+
+export default home;
---
+++ src/screens/Home/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import {styles} from "./styles"
+
+export default function Home({title}) {
+  return <>
+      <Text {...title}styles={styles.container}></Text>
+      </>
+}
+
