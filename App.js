d84aef057461234c01a4962b1fba8ba43d65bc4a
Marco Goedert <marco.goedert@edu.pucrs.br>
2023-04-05 18:26:33 -0300

    Add offline access

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Home from './src/screens/Home';
-import mock from './src/mocks/Home'
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Home from "./src/screens/Home";
+import mock from "./src/mocks/Home";
+import { useEffect } from "react";
+import useApiData from "./src/hooks/use-api-data";
 
 export default function App() {
+  const [placesData, placesLoading, placesError] = useApiData(
+    "https://rotasrurais.free.beeceptor.com/api/v1/places",
+    "places"
+  );
+
   return (
     <View style={styles.container}>
-      <Home {...mock}/>
+      <Home {...mock} />
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>Places Data: {JSON.stringify(placesData)}</Text>
+        <Text>Places Loading: {JSON.stringify(placesLoading)}</Text>
+        <Text>Places Error: {JSON.stringify(placesError)}</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,8 +27,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- src/hooks/use-api-data.js
+++ src/hooks/use-api-data.js
@@ -1,7 +1,18 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import NetInfo from "@react-native-community/netinfo";
+import axios from "axios";
 
+/**
+ * This hook is used to fetch data from an API and store it in AsyncStorage
+ * It also checks if the device is connected to the internet
+ * If it is, it fetches the data from the API and stores it in AsyncStorage
+ * If it isn't, it fetches the data from AsyncStorage
+ *
+ * @param {*} url
+ * @param {*} key
+ * @returns  {data, loading, error}
+ */
 const useApiData = (url, key) => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
@@ -12,17 +23,23 @@ const useApiData = (url, key) => {
       setLoading(true);
       try {
         const storedData = await AsyncStorage.getItem(key);
+        console.log("storedData", storedData);
         if (storedData) {
           setData(JSON.parse(storedData));
         }
         const netInfo = await NetInfo.fetch();
+        console.log("netInfo.isConnected", netInfo.isConnected);
         if (netInfo.isConnected) {
-          const response = await fetch(url);
-          const apiData = await response.json();
+          console.log("fetching data from API");
+          const response = await axios.get(url);
+          console.log("response", response);
+          const apiData = await response.data;
+          console.log("apiData", apiData);
           setData(apiData);
           await AsyncStorage.setItem(key, JSON.stringify(apiData));
         }
       } catch (error) {
+        console.log("error", error);
         setError(error);
       } finally {
         setLoading(false);
@@ -32,7 +49,7 @@ const useApiData = (url, key) => {
     fetchData();
   }, [url, key]);
 
-  return { data, loading, error };
+  return [data, loading, error];
 };
 
 export default useApiData;
