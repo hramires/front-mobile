5257a1747936a759f0e825a6dacd6cce3bae4c0e
Martin Ferreira <martin.ferreira@e-core.com>
2023-05-07 18:00:31 -0300

    added header home component, fixed the padding problem with the header

--- src/components/HomeHeader/styles.js
+++ src/components/HomeHeader/styles.js
@@ -1,5 +1,5 @@
 import { StyleSheet, StatusBar } from "react-native";
-import { Colors, primaryHeaderColor } from "../../config/Theme";
+import { Colors, primaryHeaderColor, secondaryHeaderColor, whiteHeaderTextColor } from "../../config/Theme";
 
 const STATUSBAR_HEIGHT = StatusBar.currentHeight || 0;
 export default styles = StyleSheet.create({
@@ -8,7 +8,7 @@ export default styles = StyleSheet.create({
       display: 'flex',
     },
     header: {
-      backgroundColor: primaryHeaderColor,
+      backgroundColor: secondaryHeaderColor,
       height: 45, // Reduce the height of the header as needed
       flexDirection: 'row', // Use flexDirection to align the back button and header title in a row
       justifyContent: 'center',
@@ -18,9 +18,15 @@ export default styles = StyleSheet.create({
     headerTitle: {
         fontSize: 24,
         fontWeight: 'bold',
-        color: '#fff',
+        color: whiteHeaderTextColor,
       },
       backButton: {
+        position: 'absolute', // Position the back button absolutely
+        right: 70, // Move the back button to the left of the screen
+        marginLRight: 10, // Add marginLeft to create spacing between the back button and screen edge
+        padding: 10,
+      },
+      questionButton:{
         position: 'absolute', // Position the back button absolutely
         left: 0, // Move the back button to the left of the screen
         marginLeft: 10, // Add marginLeft to create spacing between the back button and screen edge
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -3,7 +3,8 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    marginTop: 0,
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
   },
