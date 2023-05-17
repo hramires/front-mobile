4f1194957e2a6fbd6d3d85775a9655cd39127076
Caique Steibel <caique.steibel@edu.pucrs.br>
2023-05-17 18:39:03 -0300

    EventList Filter

--- src/screens/EventList/index.js
+++ src/screens/EventList/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { FlatList, ScrollView, Text, View } from "react-native";
 import styles from "./styles";
 import TitleHeader from "../../components/TitleHeader";
@@ -8,10 +8,21 @@ import useStorage from "../../hooks/use-storage";
 
 export default function EventList({ navigation }) {
   const [events, eventsLoading, eventsError] = useStorage("event");
+  const [filteredEvents, setFilteredEvents] = useState(events);
+  const [filter, setFilter] = useState("");
 
   const onPressHandler = () => {
     navigation.goBack();
   };
+
+  const applyFilter = () => {
+    const filtered = events.filter(event => event.name.toLowerCase().includes(filter.toLowerCase()));
+    setFilteredEvents(filtered);
+  };
+
+  useEffect(() => {
+    applyFilter();
+  }, [filter, events]);
 
   return (
     <View style={styles.container}>
@@ -20,7 +31,7 @@ export default function EventList({ navigation }) {
         {eventsLoading && <Text>Loading...</Text>}
         {eventsError && <Text>Error: {eventsError}</Text>}
         <FlatList
-          data={events}
+          data={filteredEvents}
           renderItem={({ item }) => (
               <EventCard
                 title={item.name}
